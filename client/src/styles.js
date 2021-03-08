2c6dc46d59faf8b8b35fac903d804260cbfbef62
Edgar <cardenasedgar123@gmail.com>
2021-03-08 10:41:54 -0500

    changes

--- client/src/styles.js
+++ client/src/styles.js
@@ -2,15 +2,16 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
   appBar: {
-    borderRadius: 15,
+    borderRadius: '5px',
     margin: '30px 0',
     display: 'flex',
+    padding: '10px',
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
   },
   heading: {
-    color: 'rgba(0,183,255, 1)',
+    color: 'black',
   },
   image: {
     marginLeft: '15px',
